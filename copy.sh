711c7c758c8fd0ad779e2a53492f0ab0c8437bb7
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2014-09-09 15:43:13 +0200

    Delete copy.sh

--- copy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp -r ../magento19/app/code/community/Fballiano/AutodeleteProductImages app/code/community/Fballiano/
-cp -r ../magento19/app/etc/modules/Fballiano_AutodeleteProductImages.xml app/etc/modules
