b625d7761cbab02436ac7e5b6876174ffd7c6143
Fabrizio Balliano <fabrizio@fabrizioballiano.it>
2014-09-09 10:25:37 +0200

    initial commit

---
+++ copy.sh
@@ -0,0 +1,2 @@
+cp -r ../magento19/app/code/community/Fballiano/AutodeleteProductImages app/code/community/Fballiano
+cp -r ../magento19/app/etc/modules/Fballiano_AutodeleteProductImages.xml app/etc/modules/
